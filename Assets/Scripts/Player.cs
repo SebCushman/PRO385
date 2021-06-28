b42af3679b00abb8bb8b07b89692476bffb5fa2f
SebCushman <61478231+SebCushman@users.noreply.github.com>
2021-06-28 10:55:08 -0600

    Revert "Revert "First Class Stuff""
    This reverts commit 697eea4bb2eab4707f07ecfbb8df5777d72a004a.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2;
+    public GameObject shot;
+    
+    Vector2 input;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        transform.Translate(input * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            OnFire();
+        }
+    }
+
+    void OnFire()
+    {
+        Instantiate(shot, transform.position, Quaternion.identity);
+    }
+}
